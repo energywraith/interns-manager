80a0fed76696170102a0321cadcbe3e8381c0d33
Adam Jałocha <jalochadev@gmail.com>
2023-08-09 01:23:52 +0200

    Added user avatar validation

--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -27,8 +27,17 @@ const firstName = ref(user.value?.first_name || '')
 const lastName = ref(user.value?.last_name || '')
 const avatar = ref(user.value?.avatar || '')
 
+const avatarError = ref('')
+
 const onUserCreateHandle = async (e: SubmitEvent) => {
   e.preventDefault()
+
+  if (!avatar.value) {
+    avatarError.value = 'Image is required'
+    return
+  }
+
+  avatarError.value = ''
 
   if (isEdit && userId) {
     await usersInject?.onUserUpdate({
@@ -48,13 +57,23 @@ const onUserCreateHandle = async (e: SubmitEvent) => {
   router.push({ path: '/' })
 }
 
-const onAvatarAdd = () => {
+const onAvatarAdd = async () => {
   const avatarLink = prompt('Please enter avatar link', 'https://reqres.in/img/faces/1-image.jpg')
 
   if (!avatarLink) {
     return
   }
 
+  const res = await fetch(avatarLink)
+  const buff = await res.blob()
+
+  if (!buff.type.startsWith('image/')) {
+    avatarError.value = 'Image is not valid'
+    avatar.value = ''
+    return
+  }
+
+  avatarError.value = ''
   avatar.value = avatarLink
 }
 </script>
@@ -62,7 +81,7 @@ const onAvatarAdd = () => {
 <template>
   <form :onsubmit="onUserCreateHandle">
     <main>
-      <h1>Add user</h1>
+      <h1>{{ isEdit ? 'Update' : 'Add' }} user</h1>
       <ShadowCard class="name-card">
         <InputComponent
           v-model="firstName"
@@ -76,17 +95,29 @@ const onAvatarAdd = () => {
           label="Last Name"
           required
         />
-        <ButtonComponent class="name-card__button" type="submit">Update Details</ButtonComponent>
+        <ButtonComponent class="name-card__button" type="submit"
+          >{{ isEdit ? 'Update' : 'Save' }} Details</ButtonComponent
+        >
       </ShadowCard>
       <ShadowCard class="avatar-card">
-        <AvatarComponent class="avatar-card__avatar" :src="avatar" />
+        <AvatarComponent
+          class="avatar-card__avatar"
+          :src="avatar"
+          :class="{
+            error: !!avatarError
+          }"
+        />
+        <span class="avatar-card__error">{{ avatarError }}</span>
         <ButtonComponent
-          :onclick="onAvatarAdd"
-          :icon="IconCamera"
           type="button"
           class="avatar-card__button"
           variant="ghost"
-          >Change Photo</ButtonComponent
+          :onclick="onAvatarAdd"
+          :icon="IconCamera"
+          :class="{
+            error: !!avatarError
+          }"
+          >{{ avatar ? 'Change' : 'Add' }} Photo</ButtonComponent
         >
       </ShadowCard>
     </main>
@@ -140,7 +171,7 @@ main {
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  row-gap: 6rem;
+  row-gap: 1rem;
   padding-top: 4rem;
   order: 1;
 
@@ -151,11 +182,28 @@ main {
   &__avatar {
     height: 10rem;
     width: 10rem;
+
+    &.error {
+      border: 1px solid var(--color-red-600);
+    }
+  }
+
+  &__error {
+    height: 1rem;
+    justify-self: flex-start;
+    color: var(--color-red-600);
+    text-shadow: 0 0 1px var(--color-dark-100);
+    font-weight: 600;
   }
 
   &__button {
     justify-self: flex-end;
     width: 100%;
+    margin-top: 5rem;
+
+    &.error {
+      border: 1px solid var(--color-red-600);
+    }
   }
 }
 </style>
